c2c32bc3757616e4076d5aea85c0f2774c3cf68e
Patrick Hill <graffixnyc@gmail.com>
2023-10-01 18:44:39 -0400

    Added some graphql error handling

--- graphql/server/resolvers.js
+++ graphql/server/resolvers.js
@@ -6,6 +6,7 @@ import {
 } from './config/mongoCollections.js';
 
 import {v4 as uuid} from 'uuid'; //for generating _id's
+
 /* parentValue - References the type def that called it
     so for example when we execute numOfEmployees we can reference
     the parent's properties with the parentValue Paramater
@@ -23,8 +24,9 @@ export const resolvers = {
       const employers = await employerCollection();
       const employer = await employers.findOne({_id: args._id});
       if (!employer) {
+        //can't find the employer
         throw new GraphQLError('Employer Not Found', {
-          extensions: {code: '404'}
+          extensions: {code: 'NOT_FOUND'}
         });
       }
       return employer;
@@ -33,8 +35,9 @@ export const resolvers = {
       const employees = await employeeCollection();
       const employee = await employees.findOne({_id: args._id});
       if (!employee) {
+        //can't find the employee
         throw new GraphQLError('Employee Not Found', {
-          extensions: {code: '404'}
+          extensions: {code: 'NOT_FOUND'}
         });
       }
       return employee;
@@ -42,11 +45,23 @@ export const resolvers = {
     employers: async () => {
       const employers = await employerCollection();
       const allEmployers = await employers.find({}).toArray();
+      if (!allEmployers) {
+        //Could not get list
+        throw new GraphQLError(`Internal Server Error`, {
+          extensions: {code: 'INTERNAL_SERVER_ERROR'}
+        });
+      }
       return allEmployers;
     },
     employees: async () => {
       const employees = await employeeCollection();
       const allEmployees = await employees.find({}).toArray();
+      if (!allEmployees) {
+        //Could not get list
+        throw new GraphQLError(`Internal Server Error`, {
+          extensions: {code: 'INTERNAL_SERVER_ERROR'}
+        });
+      }
       return allEmployees;
     }
   },
@@ -78,6 +93,7 @@ export const resolvers = {
   Mutation: {
     addEmployee: async (_, args) => {
       const employees = await employeeCollection();
+      const employers = await employerCollection();
 
       const newEmployee = {
         _id: uuid(),
@@ -85,17 +101,37 @@ export const resolvers = {
         lastName: args.lastName,
         employerId: args.employerId
       };
-      await employees.insertOne(newEmployee);
+      //make sure they entered a valid employer ID
+      let employer = await employers.findOne({_id: args.employerId});
+      if (!employer) {
+        throw new GraphQLError(
+          `Could not Find Employer with an ID of ${args.employerId}`,
+          {
+            extensions: {code: 'BAD_USER_INPUT'}
+          }
+        );
+      }
+      let insertedEmployee = await employees.insertOne(newEmployee);
+      if (!insertedEmployee.acknowledged || !insertedEmployee.insertedId) {
+        throw new GraphQLError(`Could not Add Employee`, {
+          extensions: {code: 'INTERNAL_SERVER_ERROR'}
+        });
+      }
       return newEmployee;
     },
     removeEmployee: async (_, args) => {
       const employees = await employeeCollection();
-      const oldEmployee = await employees.findOne({_id: args._id});
-      const deletionInfo = await employees.deleteOne({_id: args._id});
-      if (deletionInfo.deletedCount === 0) {
-        throw `Could not delete user with _id of ${args._id}`;
+      const deletedEmployee = await employees.findOneAndDelete({_id: args._id});
+
+      if (!deletedEmployee) {
+        throw new GraphQLError(
+          `Could not delete employee with _id of ${args._id}`,
+          {
+            extensions: {code: 'NOT_FOUND'}
+          }
+        );
       }
-      return oldEmployee;
+      return deletedEmployee;
     },
     editEmployee: async (_, args) => {
       const employees = await employeeCollection();
@@ -110,23 +146,43 @@ export const resolvers = {
         }
         if (args.employerId && args.employerId > 0) {
           const employers = await employerCollection();
-          const employerCount = await employers.count({});
-          if (employerCount + 1 >= args.employerId) {
+          const employerCount = await employers.count({_id: args.employerId});
+          if (employerCount === 1) {
             newEmployee.employerId = args.employerId;
+          } else {
+            throw new GraphQLError(
+              `Could not Find Employer with an ID of ${args.employerId}`,
+              {
+                extensions: {code: 'BAD_USER_INPUT'}
+              }
+            );
           }
         }
         await employees.updateOne({_id: args._id}, {$set: newEmployee});
+      } else {
+        throw new GraphQLError(
+          `Could not update employee with _id of ${args._id}`,
+          {
+            extensions: {code: 'NOT_FOUND'}
+          }
+        );
       }
       return newEmployee;
     },
     addEmployer: async (_, args) => {
+      //check args
       const employers = await employerCollection();
       const employerCount = await employers.count({});
       const newEmployer = {
         _id: employerCount + 1,
         name: args.name
       };
-      await employers.insertOne(newEmployer);
+      let insertEmployer = await employers.insertOne(newEmployer);
+      if (!insertEmployer.acknowledged || !insertEmployer.insertedId) {
+        throw new GraphQLError(`Could not Add Employer`, {
+          extensions: {code: 'INTERNAL_SERVER_ERROR'}
+        });
+      }
       return newEmployer;
     }
   }
