0580f3c9deb3d8c6bdc8ba775c47b7bc26350c3e
Patrick Hill <graffixnyc@gmail.com>
2023-10-09 17:19:57 -0400

    Added another React example

---
+++ react_first_lecture/simple-component-example/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Hello from './Hello.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+      <Hello/>
+      <Hello/>
+      <Hello/>
+      <Hello/>
+      <Hello/>
+    </>
+  )
+}
+
+export default App
