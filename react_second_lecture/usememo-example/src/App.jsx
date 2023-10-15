b92219b47f873e6c7362c8769eae261d753f3763
Patrick Hill <graffixnyc@gmail.com>
2023-10-15 15:44:02 -0400

    Updated useMemo example

---
+++ react_second_lecture/usememo-example/src/App.jsx
@@ -0,0 +1,14 @@
+import NoMemo from './NoMemo';
+import Memo from './Memo'
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <NoMemo />
+      <Memo/>
+    </div>
+  );
+}
+
+export default App;
